c82cf85f7ac3d57c89f8ba3e50e8814335c4c23a
fengandong <ufiread@sina.com>
2020-12-25 21:43:38 +0800

    新增github模块

---
+++ ufire-github/src/main/java/com/github/ufire/UfireGithubApplication.java
@@ -0,0 +1,14 @@
+package com.github.ufire;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @description:
+ * @author: fengandong
+ * @time: 2020/12/25 21:41
+ */
+public class UfireGithubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UfireGithubApplication.class);
+    }
+}
