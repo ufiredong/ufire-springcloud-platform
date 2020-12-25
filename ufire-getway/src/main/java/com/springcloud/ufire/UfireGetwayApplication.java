d7bf0a00505184f001eaf6825fcaf6bc21dd9046
fengandong <ufiread@sina.com>
2020-12-25 21:16:39 +0800

    搭建微服-注册中心、网关

---
+++ ufire-getway/src/main/java/com/springcloud/ufire/UfireGetwayApplication.java
@@ -0,0 +1,11 @@
+package com.springcloud.ufire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfireGetwayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UfireGetwayApplication.class);
+    }
+}
