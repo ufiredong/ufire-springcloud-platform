e19f8092ce19f048204adaa861c6bebadfba5094
ufireadong <ufiread@sina.com>
2021-01-15 18:06:48 +0800

    新增core模块 mybatis-generator 工具

--- ufire-getway/src/main/java/com/springcloud/ufire/getway/UfireGetwayApplication.java
+++ ufire-getway/src/main/java/com/springcloud/ufire/getway/UfireGetwayApplication.java
@@ -1,7 +1,10 @@
 package com.springcloud.ufire.getway;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@MapperScan({"com.springcloud.ufire.core.mapper"})
 @SpringBootApplication
 @EnableDiscoveryClient
 public class UfireGetwayApplication {
